5bff87f3a4350522e0d7b52434a10c591b5ff6fa
jaker0889-lang <jaker0889@gmail.com>
2026-07-29 13:51:06 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -20,21 +20,13 @@ window.onload = function () {
 const message = `Hi babyyyy, cheeky, kyraa
 
 I made this for you because I know you deserve this, and I want to show you how important you are to me.
-
 It's been 2 months since we started talking, and I still can't believe how far we've come.
-
 We met unexpectedly on Litmatch, and I never thought that I would get to know you this much, become close with you, and have someone like you in my life.
-
 At first, I thought our conversations would only be temporary. But as we spent more time talking, watching movies, and playing online games together, my heart slowly got attached to you until I realized that I was falling in love with you.
-
 I want you to know that I'm serious about you. I want you to stay by my side, and I hope you'll be someone I can share my life with.
-
 Thank you for choosing to understand me. Even when we don't always agree, you still stay by my side. Thank you for comforting me whenever I have problems and for not giving up on me.
-
 I hope you know how much I appreciate you, Kyra.
-
 Alam mo, nag-sink in sa isip ko yung sinabi mo kagabi. You're right. I don't want to waste this opportunity, and I'm scared of losing you.
-
 That's why I want to be honest with you and show you how much you mean to me.`;
 
 let index = 0;
